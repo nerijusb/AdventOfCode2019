8c2d182ef530dd6a8bf5a725b48d7ef3422a8237
Nerijus <nerijus.bucinskis@gmail.com>
2019-12-07 18:53:58 +0200

    Day 5 solved

---
+++ Day05_2.java
@@ -0,0 +1,12 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2019/day/5
+ *
+ * @author Nerijus
+ */
+public class Day05_2 extends Day05_1 {
+    public static void main(String[] args) {
+        System.out.println("Diagnostic code for system ID 5:");
+        new Day05_1.IntCodeComputer(5).run();
+    }
+}
